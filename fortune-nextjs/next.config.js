34c021b5bba4cab92018cdc80516701181481eb3
Nana Kamoshita <nana.kamoshita@system-exe.jp>
2025-07-04 15:10:19 +0900

    fix: GitHub Pages用のbasePathを修正

--- fortune-nextjs/next.config.js
+++ fortune-nextjs/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/fortune-nextjs' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/nana-kamoshita-0702' : '',
   images: {
     unoptimized: true,
   },
